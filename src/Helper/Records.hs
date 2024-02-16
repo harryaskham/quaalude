7616ea0b6179cad00308b751e6b1e37a65c7a7e1
Harry Askham <harryaskham@gmail.com>
2024-02-16 21:35:12 +0000

    record utils n snake case

---
+++ src/Helper/Records.hs
@@ -0,0 +1,13 @@
+module Helper.Records where
+
+import Data.Aeson.Casing (snakeCase)
+
+-- Convenience function to drop i.e. _llamaRequest... from field names when deriving JSON.
+-- i.e. use dropPrefix "LlamaRequest" for the above
+dropPrefix :: String -> String -> String
+dropPrefix prefix s = if prefix `isPrefixOf` s then drop (length prefix) s else s
+
+-- for use as:
+-- deriveJSON defaultOptions {fieldLabelModifier = snakeCaseNoPrefix "_recordName"} ''RecordName
+snakeCaseNoPrefix :: String -> String -> String
+snakeCaseNoPrefix prefix = snakeCase . dropPrefix prefix
