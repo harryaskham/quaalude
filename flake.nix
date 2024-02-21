7989cd823fd9a57ab0517c02133f98665955cec8
Harry Askham <harryaskham@gmail.com>
2024-02-21 12:15:30 +0000

    nix

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "quaalude";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in with pkgs; rec {
+        packages.app = haskellPackages.callCabal2nix "quaalude" ./. {  };
+        defaultPackage = packages.app;
+      }
+    );
+}
--- release.nix
+++ release.nix
@@ -1,2 +1,2 @@
 { pkgs ? import <nixpkgs> { config = { allowBroken = true; }; } }:
-pkgs.haskellPackages.callCabal2nix "" ./. {  }
+pkgs.haskellPackages.callCabal2nix "quaalude" ./. {  }
