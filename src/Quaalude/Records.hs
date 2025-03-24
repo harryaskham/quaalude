a0e5a60c17ce189b2945335337e6d19edc519a89
Harry Askham <harryaskham@gmail.com>
2025-03-24 11:00:13 +0000

    UnRecord type

--- src/Quaalude/Records.hs
+++ src/Quaalude/Records.hs
@@ -1,6 +1,8 @@
 module Quaalude.Records where
 
 import Data.Aeson.Casing (snakeCase)
+import Data.HList (Record, ErrShowType)
+import GHC.TypeLits
 
 -- Convenience function to drop i.e. _llamaRequest... from field names when deriving JSON.
 -- i.e. use dropPrefix "LlamaRequest" for the above
@@ -11,3 +13,8 @@ dropPrefix prefix s = if prefix `isPrefixOf` s then drop (length prefix) s else 
 -- deriveJSON defaultOptions {fieldLabelModifier = snakeCaseNoPrefix "_recordName"} ''RecordName
 snakeCaseNoPrefix :: String -> String -> String
 snakeCaseNoPrefix prefix = snakeCase . dropPrefix prefix
+
+-- Unwrap a Record to its underlying typelevel list
+type family UnRecord record :: [*]where
+  UnRecord (Record r) = r
+  UnRecord e = TypeError ('Text "UnRecord: " ':<>: 'ShowType e)
