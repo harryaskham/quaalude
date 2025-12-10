e1e401f6f739d89783f6a7ecb1344dd7d42d92dd
Harry Askham <harryaskham@gmail.com>
2025-12-10 13:36:51 +0000

    [rapid commit; squash later]

--- src/Quaalude/Lists.hs
+++ src/Quaalude/Lists.hs
@@ -1,6 +1,8 @@
 module Quaalude.Lists where
 
 import Data.HList
+import GHC.TypeNats
+import Quaalude.Alias
 
 class Nothings l where
   nothings :: HList l
@@ -10,3 +12,85 @@ instance Nothings '[] where
 
 instance (Nothings xs) => Nothings (Maybe x' ': xs) where
   nothings = HCons Nothing (nothings @xs)
+
+data HomList (n :: Nat) a where
+  HomNil :: forall {a}. HomList 0 a
+  HomCons :: forall n {a}. a -> HomList (n - 1) a -> HomList n a
+
+type family HomConsF a b where
+  HomConsF a a = HomList 2 a
+  HomConsF a (HomList n a) = HomList (n + 1) a
+
+type family HomConsEvidenceF n a where
+  HomConsEvidenceF 2 a =
+    (HomConsF a a ~ HomList 2 a)
+  HomConsEvidenceF n (HomList n a) =
+    ((n + 1) - 1 ~ n, HomConsF a (HomList n a) ~ HomList (n + 1) a)
+
+type family HomConsEvidenceC na :: Constraint where
+  HomConsEvidenceC (SNat n, a) = HomConsEvidenceF n a
+
+type family HomConsEvidenceT na
+
+class (HomConsEvidenceC (HomConsEvidenceT (SNat n, a))) => HomConsEvidence n a
+
+instance (HomConsEvidenceC (HomConsEvidenceT (SNat n, a))) => HomConsEvidence n a
+
+type family HomConsLHS b where
+  HomConsLHS (HomList n a) = a
+  HomConsLHS a = a
+
+class (n ~ (n + 1 - 1)) => NatSuccPred n
+
+instance (n ~ (n + 1 - 1)) => NatSuccPred n
+
+class HomConsC a where
+  (~:~) :: (NatSuccPred n) => a -> HomList n a -> HomList (n + 1) a
+  (~:~) = HomCons
+  (~:|) :: a -> a -> HomList 2 a
+  a ~:| b = HomCons a (HomCons b HomNil)
+
+instance HomConsC a
+
+infixr 5 ~:~
+
+data Question (c :: * -> Constraint) a = Question a
+
+type a :~:~ b = HomConsF a b
+
+infixr 5 :~:~
+
+class ConstC (cf :: (k -> Constraint)) (a :: k)
+
+instance (cf a) => ConstC cf a
+
+type family AllEqF a :: * -> Constraint where
+  AllEqF (HomList n a) = ConstC AllEq
+
+type family a :≡ b where
+  a :≡ () = Question AllEq (HomList 1 a)
+  a :≡ Question c (HomList n a) = Question c (HomList (n + 1) a)
+
+infix 4 :≡
+
+class AllEq a where
+  allEq :: a -> Bool
+
+instance {-# OVERLAPS #-} AllEq (HomList 0 a) where
+  allEq _ = True
+
+instance {-# OVERLAPS #-} (Eq a, AllEq (HomList (n - 1) a)) => AllEq (HomList n a) where
+  allEq (HomCons a rest@(HomCons b _)) = a ≡ b ∧ allEq @(HomList (n - 1) a) rest
+  allEq (HomCons a HomNil) = True
+
+type family QuestionResultF c where
+  QuestionResultF AllEq = Bool
+
+type family RunQuestionC q :: Constraint where
+  RunQuestionC (Question c a) = c a
+
+type family RunQuestionF q where
+  RunQuestionF (Question c a) = a -> QuestionResultF c
+
+class RunQuestion q where
+  runQuestion :: (RunQuestionC q) => RunQuestionF q
